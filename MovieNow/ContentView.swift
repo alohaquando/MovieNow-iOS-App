e3c6d24b3c7a64e92d3fb0ec6155032cca4818c7
aloha.quan <aloha.quando@gmail.com>
2022-08-30 13:50:52 +0700

    Initial Commit

---
+++ MovieNow/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MovieNow
+//
+//  Created by Quân Đỗ on 8/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieNow/MovieNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieNowApp.swift
+//  MovieNow
+//
+//  Created by Quân Đỗ on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
